4ad21f8ab565f0baac95d29f2f76e61e5c326a8c
olton <serhii@pimenov.com.ua>
2022-12-12 20:52:51 +0200

    added demo

---
+++ demo/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+import "./index.less"
+
+const App = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App />, );
